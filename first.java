389bf0c3c6a2ed5fb027a80d52cb2a0f1b805194
dev razz <masterrazz363@gmail.com>
2023-09-24 21:26:24 -0400

    modified first.java

--- first.java
+++ first.java
@@ -1,4 +1,5 @@
-public class first{
+import java.lang.*; // this lang is libarary that cantain so many function/method lile System.out and etc if you don't import this it will auto imported
+ class first{
    public static void main(String arg[]){
     System.out.println("Hello worlds!");
    }
