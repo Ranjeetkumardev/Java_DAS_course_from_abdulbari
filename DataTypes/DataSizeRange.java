e6bfde3c99776e8133df158cdc551ba8e1864787
dev razz <masterrazz363@gmail.com>
2023-09-25 11:20:24 -0400

    modified main file location

---
+++ DataTypes/DataSizeRange.java
@@ -0,0 +1,25 @@
+import util.*;
+/* Data Types 
+1.premitive Data Types 
+i.Integer
+  a.byte size 1 range -128 to 127 default 0
+  b.short - size 2 range -32768 to 32767 default 0
+  c. int - size 4 range -2147483648 to 2147483647  default 0
+  d.long -> size 8 ---         default 0
+ii.Float point
+  a.float - size 4 range +-1.4E-45 to +-3.4E+38 default 0.0f
+  b.double -size 8 range +-439E-324 to +-1.7E+308 default 0.0d
+iii.char size 2 range 0 to .65535 // bcz java supports unicode i.e java supports other language as well apart from english(ASCII CODE) like japani,french,etc ASCII CODE is subset of unicode 
+ 
+vi.boolean size ? range true/flase default false // in java true/false meanse true/false but other lang true=1 /false =0 but in java not like that 
+
+1.Non premitive Data Types 
+ i. Array 
+ ii.linkList etc */
+ 
+public class Data_type {
+
+    public static void main(){
+
+    }
+}
