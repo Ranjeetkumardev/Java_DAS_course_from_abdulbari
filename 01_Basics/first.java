194c6361ad1ec75aa5dc78a46210d64ef36559a2
dev razz <masterrazz363@gmail.com>
2023-09-24 21:43:31 -0400

    modified first.java

---
+++ 01_Basics/first.java
@@ -0,0 +1,10 @@
+import java.lang.*; // this lang is libarary that cantain so many function/method like System.out and etc if you don't import this it will auto imported
+ //class first{
+ public class first{   // class name and file name should be same //when this is mendatory when class is created public
+   // String arg[] -> comind line arguments -> if you odnt write this error:-Main method not found in class first, please define the main method as: public static void main(String[] args) that means jvm wants main method in this signature only
+
+   public static void main(){
+    System.out.println("Hello worlds!");
+
+   }
+}
